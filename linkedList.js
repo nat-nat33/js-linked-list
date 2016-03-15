9d16e9af4eb45c63e61e567039e1b7257d7026ef
Juan Jose Gonzalez Macias <JuanJo@MacBook-Pro-de-Juan-Jose-Gonzalez-Macias.local>
2016-03-14 20:29:33 -1000

    finished linked list

--- linkedList.js
+++ linkedList.js
@@ -32,18 +32,85 @@ function linkedListGenerator(){
 			return newNode;
 		}
 
-	function remove(){
-			return null;
+	function remove(n){
+		var count = 0;
+		var match = false;
+
+		if(head === null){
+			return false;
+		}
+		if(n === 0){
+			var aHead = head;
+			head = head.next;
+			if(head === null){
+			tail = null;
+		}
+			return aHead;
 		}
 
-	function get(number){
+	
+		var currentNode = head;
+		while(currentNode.next !== null){
+			count ++;
+			currentNode = currentNode.next;
+
+			if(count === n){
+				match= true;
+				var previous = get(n -1);
+				
+			if(get(n).next === null){
+				previous.next = null;
+				tail = previous;
+			}
+			else {
+				previous.next = get(n + 1);
+			 }
+		 	}
+		
+		}
+		if(!match){
+			return match;
+	}
+}
+	
+
+	function get(n){
+		
+		var count = 0;
+		var current = head;
+
+		if(n === 0){
 			return head;
 		}
+		
+		var currNode = head;
+		while (currNode.next !== null){
+			 count++;
+			 currNode = currNode.next;
 
-	function insert(){
-			return null;
+			if(count === n){
+				return currNode;
+			}
+		} 
+		return false;
 
 	}
+
+	function insert(value, index){
+		var previous = get(index -1);
+		var nodeAdd= {
+			value: value,
+			next: get(index)
+		};
+
+		previous.next = nodeAdd;
+
+		if(index === 0){
+			head = nodeAdd;
+		} else {
+		return false;
+	}
+}
 
 return {
 	getHead: getHead,
